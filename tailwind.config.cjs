58d46167396ee35560218815c9f4bb5079447b01
Juan Manuel Betancur <juanbetancur142474@correo.itm.edu.co>
2026-01-11 11:27:52 -0500

    Downgrade de tailwind exitoso

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,7 @@
 module.exports = {
   content: [
     "./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}",
-    "./public/**/*.html",
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [
     require('@tailwindcss/typography'),
   ],
